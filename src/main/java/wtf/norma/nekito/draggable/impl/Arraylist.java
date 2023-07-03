b0c32dbf57071409a555065fae69dceb3362a6c7
Marcus <121552016+intexpression@users.noreply.github.com>
2023-07-03 17:12:31 +0200

    cum

---
+++ src/main/java/wtf/norma/nekito/draggable/impl/Arraylist.java
@@ -0,0 +1,31 @@
+package wtf.norma.nekito.draggable.impl;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.util.EnumChatFormatting;
+import org.lwjgl.util.vector.Vector2f;
+import wtf.norma.nekito.draggable.AbstractDraggable;
+import wtf.norma.nekito.helper.OpenGlHelper;
+import wtf.norma.nekito.util.render.RenderUtility;
+
+import java.awt.*;
+
+public class Arraylist extends AbstractDraggable {
+
+
+    // @tecness
+    // zurb to prosze !!!!
+    // plz
+
+        @Override
+        public void Init() {
+
+            X = 0;
+            Y = 0;
+        }
+    public Vector2f Render() {
+
+        return null;
+    }
+
+}
