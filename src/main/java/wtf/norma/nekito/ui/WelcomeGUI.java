94cabc6955f6c156652990db73ce20ba34bb16be
groszus <79080481+groszus@users.noreply.github.com>
2023-07-03 14:31:02 +0200

    Update WelcomeGUI.java

--- src/main/java/wtf/norma/nekito/ui/WelcomeGUI.java
+++ src/main/java/wtf/norma/nekito/ui/WelcomeGUI.java
@@ -3,6 +3,7 @@ package wtf.norma.nekito.ui;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.gui.ScaledResolution;
+import net.minecraft.util.EnumChatFormatting;
 import wtf.norma.nekito.util.font.Fonts;
 import wtf.norma.nekito.util.render.CwelUtility;
 import wtf.norma.nekito.util.render.RenderUtility;
@@ -19,12 +20,12 @@ public class WelcomeGUI extends GuiScreen {
             RenderUtility.drawRect(0, 0, mc.displayWidth, mc.displayHeight, new Color(0, 0, 0, 190).getRGB());
             CwelUtility.renderGray();
 
-            Fonts.SEMI_BOLD_18.drawCenteredStringWithShadow("Welcome in Nekito!", sr.getScaledWidth() / 2, sr.getScaledHeight() / 2, -1);
-            Fonts.SEMI_BOLD_14.drawCenteredStringWithShadow("Join our Discord:https://discord.gg/PXUm6zydUv ", sr.getScaledWidth() / 2, sr.getScaledHeight() / 2 + 14, -1);
-            Fonts.SEMI_BOLD_12.drawCenteredStringWithShadow("Client made by: Intexpression, groszus, lekoz and tecness ", sr.getScaledWidth() / 2, sr.getScaledHeight() / 2 + 24, -1);
-            Fonts.SEMI_BOLD_12.drawCenteredStringWithShadow("Have Fun Crashing Servers!", sr.getScaledWidth() / 2, sr.getScaledHeight() / 2 + 44, -1);
+            Fonts.SEMI_BOLD_18.drawCenteredStringWithShadow("Welcome in Nekito!", sr.getScaledWidth() / 2, sr.getScaledHeight() / 2 - 30, -1);
+            Fonts.SEMI_BOLD_16.drawCenteredStringWithShadow("Join our Discord: https://discord.gg/PXUm6zydUv ", sr.getScaledWidth() / 2, sr.getScaledHeight() / 2 - 14, -1);
+            Fonts.SEMI_BOLD_16.drawCenteredStringWithShadow("Client made by: Intexpression, groszus, lekoz and tecness ", sr.getScaledWidth() / 2, sr.getScaledHeight() / 2, -1);
+            Fonts.SEMI_BOLD_16.drawCenteredStringWithShadow(EnumChatFormatting.UNDERLINE + "Have Fun Crashing Servers! ", sr.getScaledWidth() / 2 + 1, sr.getScaledHeight() / 2 + 30, -1);
 
-            Fonts.SEMI_BOLD_12.drawCenteredStringWithShadow("To leave this screen click 'ESC'", sr.getScaledWidth() / 2, sr.getScaledHeight() - 10, -1);
+            Fonts.SEMI_BOLD_16.drawCenteredStringWithShadow("To leave this screen click 'ESC'", sr.getScaledWidth() / 2, sr.getScaledHeight() - 15, -1);
         } catch (Exception e) {
             e.printStackTrace();
         }
