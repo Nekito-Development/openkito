2af436badfc83e9c711e61f26c4caedca68465b0
Markus Ostryński <121552016+intexpression@users.noreply.github.com>
2023-08-14 00:06:27 +0200

    deleted that shit

--- src/main/java/wtf/norma/nekito/util/render/Stencil.java
+++ src/main/java/wtf/norma/nekito/util/render/Stencil.java
@@ -1,18 +1,8 @@
 package wtf.norma.nekito.util.render;
 
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.ScaledResolution;
-import net.minecraft.client.renderer.GlStateManager;
-import net.minecraft.client.shader.Framebuffer;
-import org.lwjgl.opengl.EXTFramebufferObject;
-import org.lwjgl.opengl.GL11;
-import wtf.norma.nekito.util.Util;
-
-import static org.lwjgl.opengl.GL11.glAlphaFunc;
-import static org.lwjgl.opengl.GL11C.*;
 
 
-public class Stencil implements Util {
+public class Stencil  {
 
 
 
